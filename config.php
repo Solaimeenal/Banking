7396b1ce62bce7c2253e7266a7e435b123d6e5e8
Solaimeenal <79513453+Solaimeenal@users.noreply.github.com>
2021-07-13 13:27:17 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+	$conn = mysqli_connect('localhost','id17205857_root	','y$@lA=!3v?J4K~c=','id17205857_banking');
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
